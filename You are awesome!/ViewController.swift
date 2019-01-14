bc2b9bada2ceb3d065d0393ef1fc5fdb6bcaa9a0
Sarah Kim <sarah.mjk@hotmail.com>
2019-01-13 19:07:58 -0500

    Initial Commit

---
+++ You are awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You are awesome!
+//
+//  Created by Sarah Minji Kim on 1/13/19.
+//  Copyright © 2019 Sarah Minji Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
